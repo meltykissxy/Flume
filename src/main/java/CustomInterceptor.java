17c1a645b574da2d1e322bcd0cab03951ab609a7
meltykissxy <meltykiss@yahoo.com>
2021-02-18 15:15:36 +0800

    Update CustomInterceptor.java

--- src/main/java/CustomInterceptor.java
+++ src/main/java/CustomInterceptor.java
@@ -4,8 +4,11 @@ import org.apache.flume.interceptor.Interceptor;
 
 import java.util.List;
 /**
-* 什么情况下使用initialize和close方法呢？
+* 1. 什么情况下使用initialize和close方法呢？
 * 在拦截器中读取外部数据例如Redis等数据时，在initialize中建立连接，在close中关闭连接
+* 2. 为什么有两个intercept？
+* 第一个是按个处理的，第二个按批处理，只需要循环调用第一个就可以。
+* Source有的有batchSize，有的没有，intercept重载就是解决不同的场景
 */
 public class CustomInterceptor implements Interceptor {
 
